61c7bcb7e7e88006437655cd0a4f68c96adc6888
maksudpranto <maksudpranto@gmail.com>
2024-09-13 18:14:19 +0600

    Added Add To cart & remove from cart features

--- pages/productListPage.py
+++ pages/productListPage.py
@@ -4,6 +4,9 @@ class ProductListPage:
         self._products_header = page.locator("//span[@class='title']")
         self._hamburger_menu = page.locator("//button[@id='react-burger-menu-btn']")
         self._logout_button = page.locator("//a[@id='logout_sidebar_link']")
+        # Select a Product and add to cart
+        self._addToCart_labs_bike_light = page.locator("#add-to-cart-sauce-labs-bike-light")
+        self._remove_from_cart_labs_bike_light = page.locator("//button[@id='remove-sauce-labs-bike-light']")
 
 
 
@@ -23,3 +26,9 @@ class ProductListPage:
     def product_header(self):
         return self._products_header
 
+    def add_bike_light(self):
+        self._addToCart_labs_bike_light.click()
+
+    def remove_bike_light(self):
+        self._remove_from_cart_labs_bike_light.click()
+        
