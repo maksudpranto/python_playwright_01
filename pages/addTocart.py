ca6badbbd8bbef54efa7cefe9e1fb72b08c5e8f4
maksudpranto <maksudpranto@gmail.com>
2024-09-13 22:06:10 +0600

    Completed end to end journey. Login to Checkout

--- pages/addTocart.py
+++ pages/addTocart.py
@@ -2,7 +2,7 @@
 
 class AddToCart():
     def __init__(self,page):
-        self.page = page()
+        self.page = page
         self._addToCart_labs_bike_light = page.locator("#add-to-cart-sauce-labs-bike-light")
         self._remove_from_cart_labs_bike_light = page.locator("//button[@id='remove-sauce-labs-bike-light']")
 
